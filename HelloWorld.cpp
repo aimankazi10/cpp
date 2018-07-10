b0bcaed31adf2c5c5710ce874158a70568d2c7d9
Aiman Kazi <38583336+aimankazi10@users.noreply.github.com>
2018-07-10 18:54:41 +0530

    Add files via upload
    Simple Hello World Programs for beginners

---
+++ HelloWorld.cpp
@@ -0,0 +1,25 @@
+/*
+ * Filename: main.cpp
+ * Project : c plus plus
+ * Created Date: Tuesday, July 10th 2018, 6:44:01 pm
+ * Author: Aiman Kazi
+ * 
+ * Copyright (c) 2018 Visual Labs
+ */
+
+//WAP - Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints heelo world
+    cout << "Hello World" << endl;
+
+    return 0;
+}
